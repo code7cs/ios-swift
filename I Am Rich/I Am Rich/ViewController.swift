15992d1dda6dd6eb95f7b77fc64e8f5ecd444ff2
code7cs <code7cs@gmail.com>
2020-12-06 21:38:31 -0500

    init

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Hanfan on 12/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
